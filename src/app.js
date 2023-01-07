85e0685cf03d0a29940e4adfef03f675565dd7cd
xfontainex <maralafontaine@gmail.com>
2023-01-07 02:31:07 +0000

    Added colors, a new card button, a timeout function, and made my code much more succinct

--- src/app.js
+++ src/app.js
@@ -2,75 +2,41 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const suit = ["hearts", "diamonds", "spades", "clubs"];
+const cardNum = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
+let card = document.querySelector(".card");
+let cardNumber = document.querySelector(".card-number");
+
+function getRndInteger(array) {
+  return Math.floor(Math.random() * array.length);
+}
 
 window.onload = function() {
-  // pick a suit
-  let icon;
-  switch (Math.floor(Math.random() * 4) + 1) {
-    case 1:
-      icon = "♥";
-      break;
-    case 2:
-      icon = "♦";
-      break;
-    case 3:
-      icon = "♣";
-      break;
-    case 4:
-      icon = "♠";
-  }
-
-  // pick a card
-  let cardNumber;
-  switch (Math.floor(Math.random() * 12) + 1) {
-    case 1:
-      cardNumber = "A";
-      break;
-    case 2:
-      cardNumber = 2;
-      break;
-    case 3:
-      cardNumber = 3;
-      break;
-    case 4:
-      cardNumber = 4;
-      break;
-    case 5:
-      cardNumber = 5;
-      break;
-    case 6:
-      cardNumber = 6;
-      break;
-    case 7:
-      cardNumber = 7;
-      break;
-    case 8:
-      cardNumber = 8;
-      break;
-    case 9:
-      cardNumber = 9;
-      break;
-    case 10:
-      cardNumber = "J";
-      break;
-    case 11:
-      cardNumber = "Q";
-      break;
-    case 12:
-      cardNumber = "K";
-      break;
-  }
-
-  console.log(icon, cardNumber);
-
-  document.getElementsByClassName("icon")[0].innerHTML = icon;
-
-  document.getElementsByClassName("cardNumber")[0].innerHTML = cardNumber;
-
-  if (icon == "♥") {
-    var element = document.getElementsByClassName("icon");
-    element.style.color = "red;";
-  }
+  card.classList.add(suit[getRndInteger(suit)]);
+  cardNumber.innerHTML = cardNum[getRndInteger(cardNum)];
 };
+
+window.hitMe = function hitMe() {
+  card.classList.remove(card.classList[1]);
+
+  card.classList.add(suit[getRndInteger(suit)]);
+
+  cardNumber.innerHTML = cardNum[getRndInteger(cardNum)];
+};
+
+setInterval(hitMe, 10000);
